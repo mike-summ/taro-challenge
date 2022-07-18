872e763b045b5a680942bf30d624047024bf9fcb
msummersBU <s5206096@bournemouth.ac.uk>
2022-07-18 16:30:23 +0100

    Added missing videos and fixed code

--- components/youtube-embed.js
+++ components/youtube-embed.js
@@ -1,19 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import YoutubeIframe from 'react-native-youtube-iframe';
-import taro_data from '../data/taro';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import YoutubeIframe from "react-native-youtube-iframe";
+import taro_data from "../data/taro";
 
 export default function YoutubeEmbed(props) {
-    console.log(taro_data[props.index]["id"]);
-    
-    return (
-        <View>
-            <YoutubeIframe 
-                height={720}
-                width={1080}
-                play={true}
-                videoId={taro_data[props.index]["id"]}
-            />
-        </View>
-    );
+  const index = props.index + 1;
+  let id = "";
+  for (let i = 0; i < taro_data.length; i++) {
+    if (index == taro_data[i].index) {
+      id = taro_data[i].id;
+    }
+  }
+
+  return (
+    <View>
+      <YoutubeIframe height={720} width={1080} play={true} videoId={id} />
+    </View>
+  );
 }
