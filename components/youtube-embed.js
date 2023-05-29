99f837a9f0d8578ee6123c9492bd410f1c4613c5
mike-summ <96584139+mike-summ@users.noreply.github.com>
2023-05-29 22:51:18 +0100

    Update youtube-embed.js to show Kiyo videos instead of Taro videos

--- components/youtube-embed.js
+++ components/youtube-embed.js
@@ -2,13 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import YoutubeIframe from "react-native-youtube-iframe";
 import taro_data from "../data/taro";
+import kiyo_data from "../data/kiyo";
 
 export default function YoutubeEmbed(props) {
   const index = props.index + 1;
   let id = "";
-  for (let i = 0; i < taro_data.length; i++) {
-    if (index == taro_data[i].index) {
-      id = taro_data[i].id;
+  for (let i = 0; i < kiyo_data.length; i++) {
+    if (index == kiyo_data[i].index) {
+      id = kiyo_data[i].id;
     }
   }
 
